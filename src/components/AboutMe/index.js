fe0e6473454b0457e92ec52ebf477bcab7a246c4
Ruben <[connor.j.schulz@gmail.com]>
2022-08-02 21:56:34 -0400

    changed main image size

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -4,7 +4,7 @@ function AboutMe() {
     return (
         <section>
             <h1>About Me</h1>
-            <img src={require('../../assets/ruben.jpg')} alt='Ruben Schulz'></img>
+            <img src={require('../../assets/ruben.jpg')} alt='Ruben Schulz' className='aboutme-img'></img>
             <p>
                 My name is Ruben Schulz <br></br>
                 I'm a full stack web developer and musician
